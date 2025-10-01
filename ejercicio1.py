f6d44236a347a94210cd219e9944d8464eee3448
Perez Elías <eg378556@gmail.com>
2025-10-01 09:38:54 -0300

    Agregando los primeros archivos

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+# El usuario tiene que ingresar su nombre y su edad y hay que imprimir por pantalla
+
+
+nombre = input("Ingresa tu nombre: ")
+
+
+edad = int(input("Ingresa tu edad: "))
+
+
+print(f"Su nombre es: {nombre} y su edad es: {edad}")
